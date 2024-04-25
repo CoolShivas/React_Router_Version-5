cac2e032ceeeea94d2f3b18ee5736dfc8d2d7cd1
CoolShivas <shivas2710cool@gmail.com>
2024-04-26 00:47:48 +0530

    Instead_of_AnchorTag_Link_from_react_router_dom

--- react_router_v5/src/components/Header/MainHeader.jsx
+++ react_router_v5/src/components/Header/MainHeader.jsx
@@ -1,13 +1,15 @@
-
+import { Link } from "react-router-dom";
 
 const MainHeader = () => {
   return (
     <ul>
         <li>
-            <a href="/welcome"> Welcome </a>
+            {/* <a href="/welcome"> Welcome </a> */}
+            <Link to="/welcome"> Welcome </Link>
         </li>
         <li>
-            <a href="/products"> Products </a>
+            {/* <a href="/products"> Products </a> */}
+            <Link to="/products"> Products </Link>
         </li>
     </ul>
   )
