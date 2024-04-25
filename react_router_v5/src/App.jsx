216b3b31e91b87dcca106f65362aef318e295275
CoolShivas <shivas2710cool@gmail.com>
2024-04-26 00:33:11 +0530

    Importing_of_BrowserRouter_in_main.js_file_then_Route

--- react_router_v5/src/App.jsx
+++ react_router_v5/src/App.jsx
@@ -1,15 +1,27 @@
-import Navbar from "./components/Header/Navbar";
+import ProductPage from "./pages/ProductPage";
+import WelcomePage from "./pages/WelcomePage";
+import { Route } from "react-router-dom";
+// import Navbar from "./components/Header/Navbar";
 import AppName from "./components/AppName";
 
-function App(){
-  return <>
-  <header>
-<AppName></AppName>
-<Navbar></Navbar>
-  </header>
-  <main></main>
-  <footer></footer>
-  </>
+function App() {
+  return (
+    <>
+      <header>
+        {/* Starting of URL by writing http://localhost:5173/welcome */}
+        <Route path="/welcome">
+          <WelcomePage></WelcomePage>
+        </Route>
+        <Route path="/products">
+          <ProductPage></ProductPage>
+        </Route>
+        {/* Ending of URL */}
+        <AppName></AppName>
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
+  );
 }
 
 export default App;
