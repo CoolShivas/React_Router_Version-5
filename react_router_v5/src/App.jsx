268e7b8921ac6634b4bb4b8b10ca27e22d4e6c97
CoolShivas <shivas2710cool@gmail.com>
2024-04-27 01:01:41 +0530

    Use_of_Redirect_Or_Route_Error

--- react_router_v5/src/App.jsx
+++ react_router_v5/src/App.jsx
@@ -1,8 +1,9 @@
+import ErrorPage from "./pages/ErrorPage";
 import ProductDetails from "./pages/ProductDetails";
 import MainHeader from "./components/Header/MainHeader";
 import ProductPage from "./pages/ProductPage";
 import WelcomePage from "./pages/WelcomePage";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 // import Navbar from "./components/Header/Navbar";
 // import AppName from "./components/AppName";
 
@@ -25,6 +26,12 @@ function App() {
           <Route path="/products/:productId">
             <ProductDetails></ProductDetails>
           </Route>
+          {/* Use any one from Redirect or Route Error. Both can't we use parallelly obly one at a time. */}
+          <Redirect to="/welcome"></Redirect>
+
+          {/* <Route to="*">
+            <ErrorPage></ErrorPage>
+          </Route> */}
         </Switch>
 
         {/* Ending of URL */}
---
+++ react_router_v5/src/pages/ErrorPage.jsx
@@ -0,0 +1,10 @@
+const ErrorPage = () => {
+  return (
+    <>
+      <h2> 404 </h2>
+      <h3> Page not found !</h3>
+    </>
+  );
+};
+
+export default ErrorPage;
